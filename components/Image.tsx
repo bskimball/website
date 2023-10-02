18ed5ba3612d0c86e03d1a5d1d7027f8b49888cd
bskimball <brian.kimball@bdkonline.com>
2023-10-02 13:39:40 -0400

    fix eslint stuff

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,3 +1,5 @@
-const Image = ({ ...rest }) => <img {...rest} />
+import NextImage, { ImageProps } from 'next/image'
+
+const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 
 export default Image
