bc31256852397c252a16afbda7df4996e82c1a4d
bskimball <brian.kimball@bdkonline.com>
2023-10-02 06:01:29 -0400

    Initial commit

---
+++ data/projectsData.ts
@@ -0,0 +1,49 @@
+const projectsData = [
+  {
+    title: 'BDKinc website',
+    description: `The prototype for what would be the web presence of BDKinc. The original intent would be to present 
+    an interactive experience that integrates with BDK internal processes.`,
+    imgSrc: '/static/images/bdkinc-preview.png',
+    href: '/blog/bdkinc-website',
+  },
+  {
+    title: 'Modern IBMi',
+    description: `A fullstack application that uses web sockets for communications between a React frontend and a 
+    Feathers backend.`,
+    imgSrc: '/static/images/feathers-ibm-home.png',
+    href: '/blog/feathers-ibm',
+  },
+  {
+    title: 'Knex IBMi Adapter',
+    description: `A knex adapter, written in typescript, that allows you to use knex to query DB2 for i.`,
+    imgSrc: '/static/images/knex-ibmi-npm.png',
+    href: '/blog/knex-ibmi',
+  },
+  {
+    title: 'Demographic Application',
+    description: `A fullstack application that uses web sockets for communications between a React frontend and a 
+    Feathers backend.`,
+    imgSrc: '/static/images/reach-app-home.png',
+    href: '/blog/reach-app',
+  },
+  {
+    title: 'Network Mapper',
+    description: `A fullstack performant network scanning and dns lookup application that uses fastify and solid.js.`,
+    imgSrc: '/static/images/network-mapper-dashboard.png',
+    href: '/blog/network-mapper',
+  },
+  {
+    title: 'BDKcloud website',
+    description: `The marketing website/landing page for BDKcloud. This was meant to draw focus to the company's hosting capabilities.`,
+    imgSrc: '/static/images/bdkcloud-preview.png',
+    href: '/blog/bdkcloud-website',
+  },
+  {
+    title: 'Inventory Tracker',
+    description: `An inventory tracking and management application. Written in Laravel with a Vue frontend.`,
+    imgSrc: '/static/images/inventory-tracker-lookup.png',
+    href: '/blog/inventory-tracker',
+  },
+]
+
+export default projectsData
