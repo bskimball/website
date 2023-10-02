e6ba71e317f908636d560632527a11a9b11f6d74
bskimball <brian.kimball@bdkonline.com>
2023-10-02 07:00:37 -0400

    update readme and package.json

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'A portfolio site',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://brian-kimball.com',
+  siteRepo: 'https://github.com/bskimball/my-website',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: '',
