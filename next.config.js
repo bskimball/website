491c83f7fe04df5bc51810f5174e2b381ed40c97
bskimball <brian.kimball@bdkonline.com>
2023-10-03 17:11:35 -0400

    fix eslint stuff

--- next.config.js
+++ next.config.js
@@ -60,6 +60,8 @@ const securityHeaders = [
 module.exports = () => {
   const plugins = [withContentlayer, withBundleAnalyzer]
   return plugins.reduce((acc, next) => next(acc), {
+    output: 'export',
+
     reactStrictMode: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
@@ -68,14 +70,14 @@ module.exports = () => {
     images: {
       domains: ['picsum.photos'],
     },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: securityHeaders,
-        },
-      ]
-    },
+    // async headers() {
+    //   return [
+    //     {
+    //       source: '/(.*)',
+    //       headers: securityHeaders,
+    //     },
+    //   ]
+    // },
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.svg$/,
