a944fdbacf3e8d8c17238c63eee57184fe787b6a
bskimball <brian.kimball@bdkonline.com>
2023-10-15 05:58:46 -0400

    update author and favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Logo from './Logo'
 
 const Header = () => {
   return (
@@ -12,7 +13,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="hidden text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
---
+++ components/Logo.tsx
@@ -0,0 +1,7 @@
+function Logo() {
+  return (
+    <div className="w-0 h-0 border-b-[12px] border-t-[12px] border-l-[24px] border-l-emerald-500 border-t-transparent border-b-transparent"></div>
+  )
+}
+
+export default Logo
