93b35591ed277bd0c94d0a74e1c2d5a80f7e929c
bskimball <briankimball1982@gmail.com>
2026-01-04 14:14:06 -0500

    docs(images): update references to me_website_2026.jpg

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -4,7 +4,7 @@ export default function () {
   return (
     <Avatar className="w-32 h-32">
       <AvatarImage
-        src="/static/images/me_website_2025.png"
+        src="/static/images/me_website_2026.jpg"
         alt="Brian Kimball"
       />
       <AvatarFallback>Brian Kimball</AvatarFallback>
