2893b075c92836e0f5f4ec7a7163ed3cec581f50
bskimball <briankimball1982@gmail.com>
2026-01-04 10:46:24 -0500

    update featured images

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,18 @@ import { defineCollection, reference, z } from 'astro:content'
 
 const blog = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    tags: z.array(reference('tags')).optional(),
-    draft: z.boolean().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z.string().optional(),
+      heroImageOptimized: image().optional(),
+      fullImage: z.string().optional(),
+      tags: z.array(reference('tags')).optional(),
+      draft: z.boolean().optional(),
+    }),
 })
 
 const tags = defineCollection({
