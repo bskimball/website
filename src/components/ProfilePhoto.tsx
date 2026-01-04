eb8c6996882ed1c2864b6246242c2d91edb4b42f
bskimball <briankimball1982@gmail.com>
2026-01-04 14:30:51 -0500

    update profile photo and widths

--- src/components/ProfilePhoto.tsx
+++ src/components/ProfilePhoto.tsx
@@ -1,12 +1,10 @@
 import { AvatarImage, Avatar, AvatarFallback } from '@/components/ui/avatar.tsx'
+import meImg from '../assets/me_website_2026.png'
 
 export default function () {
   return (
     <Avatar className="w-32 h-32">
-      <AvatarImage
-        src="/static/images/me_website_2026.jpg"
-        alt="Brian Kimball"
-      />
+      <AvatarImage src={meImg.src} alt="Brian Kimball" />
       <AvatarFallback>Brian Kimball</AvatarFallback>
     </Avatar>
   )
