3225be25b9a713934f0ab999b3ac6f8c0c4fbc91
Brian Kimball <briankimball1982@gmail.com>
2026-07-19 15:04:18 -0400

    Improve SEO and upgrade Astro blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
+import pagefind from 'astro-pagefind'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brian-kimball.com',
   trailingSlash: 'never',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), pagefind()],
   prefetch: {
     prefetchAll: true,
   },
